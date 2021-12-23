133e0f918e32b6fa0ac1c709a79b08fb1d681609
alphak3y <alphamik80@gmail.com>
2021-12-23 10:31:25 -0600

    cleanup python script

--- cleanup_tokens.py
+++ cleanup_tokens.py
@@ -4,8 +4,10 @@ import os
 import shutil
 
 
-i = 0
-j = 0
+active = 0
+abandoned = 0
+spam = 0
+rmdirs = 0
 
 def remove_empty_dirs(path):
   for root, dirnames, filenames in os.walk('blockchains/ethereum/assets', topdown=False):
@@ -15,38 +17,21 @@ def remove_empty_dirs(path):
 for path in Path('blockchains/ethereum/assets/').rglob('info.json'): 
   with open(path, 'r') as info:
    if '"status": "active"' in info.read():
-     # i+=1
-     # print(i)
-     print(path)
-     print(info.read())
-     # print(path.parent)
-     # for file in path.parent.iterdir():
-     #  file.unlink()
-     # shutil.rmtree(path.parent)
-     # print(str(path.parent) + " removed.")
-     # path.parent.rmdir()
-
-
-for path in Path('blockchains/ethereum/assets/').rglob('info.json'): 
-  with open(path, 'r') as info:
-   if '"status": "abandoned"' in info.read():
-     # i+=1
-     # print(i)
-     print(path.parent)
-     for file in path.parent.iterdir():
-      file.unlink()
-     # shutil.rmtree(path.parent)
-     # print(str(path.parent) + " removed.")
-     # path.parent.rmdir()
+     active += 1
+   elif '"status": "abandoned"' in info.read():
+     abandoned += 1
+   elif '"status": "spam"' in info.read():
+     spam += 1
 
 for path in os.listdir('blockchains/ethereum/assets'):
-    # j+=1
-    # print(j)
-
     if os.listdir('blockchains/ethereum/assets/' + path) == []:
       os.rmdir('blockchains/ethereum/assets/' + path)
-    # if os.path.isfile(os.path.join(dir, path)):
-    #     initial_count += 1
+      rmdirs+=1
+
+print("active tokens found: " + str(active))
+print("abandoned tokens found: " + str(abandoned))
+print("spam tokens found: " + str(spam))
+print("token directories removed: " + str(rmdirs))
 
 
     
