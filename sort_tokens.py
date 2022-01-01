a5887d96e45d0b4ec20574cc2f664513d15b2de5
alphak3y <alphak3y@protonmail.com>
2022-01-01 09:07:12 -0600

    can't sort tokens by market cap rank

--- sort_tokens.py
+++ sort_tokens.py
@@ -1,5 +1,6 @@
 import time
 import json
+from operator import itemgetter
 
 # TODO: https://raw.githubusercontent.com/poolsharks-protocol/token-metadata/master/blockchains/ethereum/assets/0xfF20817765cB7f73d4bde2e66e067E58D11095C2/logo.png
 # make background transparent for all images
@@ -10,6 +11,18 @@ for chain in supported_chains:
     f = open('blockchains/' + chain + '/tokenlist.json','r')
     print(time.time())
     tokenlist = json.load(f)
-
-    for token in tokenlist['search_tokens']:
-        print(token)
+   
+    search_tokens = tokenlist['search_tokens']
+    print(len(search_tokens))
+    for token in search_tokens:
+        #print(token)
+        if 'market_cap_rank' in token.keys() and token['market_cap_rank'] == None:
+            search_tokens.remove(token)
+           # print('removed token')
+        elif 'market_cap_rank' not in token.keys():
+            print('coin found w/o market cap rank: ' + json.dumps(token))
+            search_tokens.remove(token)
+        else:
+            print(token['market_cap_rank'])
+    print(search_tokens[0].keys())
+    sorted(search_tokens,key=itemgetter('market_cap_rank'))
