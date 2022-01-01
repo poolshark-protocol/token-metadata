6b0b3e2555054bf3af032776364dae0ee2b7aba0
alphak3y <alphak3y@protonmail.com>
2021-12-31 20:43:37 -0600

    push fixed tokens; need to sort by market cap

---
+++ sort_tokens.py
@@ -0,0 +1,13 @@
+import time
+import json
+cg = CoinGeckoAPI()
+
+supported_chains = ['ethereum']
+
+for chain in supported_chains:
+    f = open('blockchains/' + chain + '/tokenlist.json','r')
+    print(time.time())
+    tokenlist = json.load(f)
+
+    for token in tokenlist['search_tokens']:
+        print(token)
--- update_tokens.py
+++ update_tokens.py
@@ -29,7 +29,10 @@ for chain in supported_chains:
             for term in search_tokens_blacklist:
                 if coin['name'].startswith(term):
                     skip_token = True
-                    print('skipped token')
+                    #print('skipped token')
+                if os.path.exists('blockchains/' + chain + '/assets/' + Web3.toChecksumAddress(coin['platforms']['ethereum'])):
+                    skip_token = True
+                    #print('token already exists')
 
             if skip_token == False:
                 contract = w3.eth.contract(Web3.toChecksumAddress(coin['platforms']['ethereum']), abi=erc20_abi)
@@ -38,38 +41,50 @@ for chain in supported_chains:
                     os.makedirs(path)
                 if not os.path.exists(path + '/info.json'):
                     token = {}
-                    try:
-                        too_many_requests = True
-                        while too_many_requests:
-                            try:
-                                time.sleep(1.3)
-                                coin_info = cg.get_coin_info_from_contract_address_by_id(chain, coin['platforms'][chain])
-                                too_many_requests = False
-                            except:
-                                print('429 - Too Many Requests: waiting 5 seconds')
-                                time.sleep(5)
-                                
+                    #try:
+                    print(coin)
+                    too_many_requests = True
+                    while too_many_requests:
+                        try:
+                            time.sleep(1.3)
+                            coin_info = cg.get_coin_info_from_contract_address_by_id(chain, coin['platforms'][chain])
+                            too_many_requests = False
+                        except:
+                            print('429 - Too Many Requests: waiting 5 seconds')
+                            time.sleep(5)
+                    try:        
                         token['name'] = contract.functions.name().call()
                         token['symbol'] = contract.functions.symbol().call()
                         token['id'] = contract.address
                         token['decimals'] = contract.functions.decimals().call()
-                        token['coingecko_url'] = 'https://www.coingecko.com/en/coins/' + coin['id']
-                        token['market_cap_rank'] = coin_info['market_cap_rank']
-                        token['24_hr_volume_usd'] = coin_info['market_data']['total_volume']['usd']
-
-                        if coin_info['public_notice'] != None:
-                          token['public_notice'] = coin_info['public_notice']
-
-                        img_data = requests.get(coin_info['image']['large']).content
-                        img = Image.open(BytesIO(img_data))
-                        img.save(path + '/logo.png')
-
-                        token['logoURI'] = 'https://raw.githubusercontent.com/poolsharks-protocol/token-metadata/master/' + path + '/logo.png'
-                        
-                        with open(path + '/info.json','w+') as token_info:
-                            token_info.write(json.dumps(token, indent=4))
                     except:
-                      print('error calling contract for ' + json.dumps(coin))
+                        print('contract function failed for ' + json.dumps(coin) + '\ncontinuing..')
+                        continue
+
+                    token['coingecko_url'] = 'https://www.coingecko.com/en/coins/' + coin['id']
+                    token['market_cap_rank'] = coin_info['market_cap_rank']
+                    
+                    if coin_info['market_data']['total_volume'] != None and coin_info['market_data']['total_volume'] != '{}' and 'usd' in coin_info['market_data']['total_volume'].keys():
+                        #print(coin_info['market_data']['total_volume'])
+                        token['24_hr_volume_usd'] =coin_info['market_data']['total_volume']['usd']
+
+                    if coin_info['public_notice'] != None:
+                        token['public_notice'] = coin_info['public_notice']
+                    try: 
+                        img_data = requests.get(coin_info['image']['large']).content
+                    except:
+                        print('bad image URL...continuing')
+                        continue
+                    img = Image.open(BytesIO(img_data))
+                    img.save(path + '/logo.png')
+
+                    token['logoURI'] = 'https://raw.githubusercontent.com/poolsharks-protocol/token-metadata/master/' + path + '/logo.png'
+                    
+                    with open(path + '/info.json','w+') as token_info:
+                        token_info.write(json.dumps(token, indent=4))
+                    # except:
+                    #   print('error calling contract for ' + json.dumps(coin))
+                    
                     
                 else:
                     print('token already exists')
@@ -78,12 +93,12 @@ for chain in supported_chains:
                         print('found file: ' + json.dumps(coin))
                     
                 tokens[chain].append(token)
-            else:
-                print('skipped token')
+            #else:
+                #print('skipped token')
 
     f = open('blockchains/' + chain + '/tokenlist.json','r')
     print(time.time())
     tokenlist = json.load(f)
-    tokenlist['search_tokens'] = tokens[chain]
+    tokenlist['search_tokens'] = tokenlist['search_tokens'] + tokens[chain]
     fw = open('blockchains' + '/ethereum' + '/tokenlist.json','w')
     fw.write(json.dumps(tokenlist, indent=4))
