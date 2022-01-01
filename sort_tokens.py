50fdd7164773efc403eb4bdb8af706a6b293d5f2
alphak3y <alphak3y@protonmail.com>
2022-01-01 08:39:14 -0600

    note to make image bg transparent

--- sort_tokens.py
+++ sort_tokens.py
@@ -1,6 +1,9 @@
 import time
 import json
 
+# TODO: https://raw.githubusercontent.com/poolsharks-protocol/token-metadata/master/blockchains/ethereum/assets/0xfF20817765cB7f73d4bde2e66e067E58D11095C2/logo.png
+# make background transparent for all images
+
 supported_chains = ['ethereum']
 
 for chain in supported_chains:
