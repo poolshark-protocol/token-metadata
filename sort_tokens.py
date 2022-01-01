5905ba55b931091209ae42a3b848c0527f98b36d
alphak3y <alphak3y@protonmail.com>
2022-01-01 16:46:39 -0600

    sort by market cap and volume

--- sort_tokens.py
+++ sort_tokens.py
@@ -1,29 +1,27 @@
 import time
 import json
 from operator import itemgetter
+from datetime import datetime
 
 # TODO: https://raw.githubusercontent.com/poolsharks-protocol/token-metadata/master/blockchains/ethereum/assets/0xfF20817765cB7f73d4bde2e66e067E58D11095C2/logo.png
 # make background transparent for all images
 
 supported_chains = ['ethereum']
 sort_by_volume = []
+sort_by_mkt_cap = []
 for chain in supported_chains:
     f = open('blockchains/' + chain + '/tokenlist.json','r')
     print(time.time())
     tokenlist = json.load(f)
-    index = 0
+
     search_tokens = tokenlist['search_tokens']
     print(len(search_tokens))
     for token in search_tokens:
         #print(token)
-        if 'market_cap_rank' in token.keys() and token['market_cap_rank'] == None:
+        if 'market_cap_rank' in token.keys() and token['market_cap_rank'] == None and '24_hr_volume_usd' in token.keys():
             sort_by_volume.append(token)
-            del search_tokens[index]
-            print('removed token')
-        elif 'market_cap_rank' not in token.keys():
-            print('coin found w/o market cap rank: ' + json.dumps(token))
-            del search_tokens[index]
-        index += 1
+        elif 'market_cap_rank' in token.keys() and token['market_cap_rank'] != None:
+            sort_by_mkt_cap.append(token)
         
         
     for token in search_tokens:
@@ -32,5 +30,10 @@ for chain in supported_chains:
       #  elif token['market_cap_rank'] == None:
             #print(json.dumps(token))
 
-    print(len(search_tokens))
-    sorted(search_tokens,key=itemgetter('market_cap_rank'))
+    sort_by_mkt_cap = sorted(sort_by_mkt_cap,key=itemgetter('market_cap_rank'))
+    sort_by_volume = sorted(sort_by_volume, key=itemgetter('24_hr_volume_usd'), reverse=True)
+    tokenlist['search_tokens'] = sort_by_mkt_cap + sort_by_volume
+    tokenlist['timestamp'] = str(datetime.now())
+    f = open('blockchains/' + chain + '/tokenlist.json','w')
+    f.write(json.dumps(tokenlist, indent=4))
+
