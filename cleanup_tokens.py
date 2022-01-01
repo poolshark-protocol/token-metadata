efc63f0ffd3927fc4eaae7aff835013ed21d734c
alphak3y <alphak3y@protonmail.com>
2022-01-01 16:59:45 -0600

    cleanup script

--- cleanup_tokens.py
+++ cleanup_tokens.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
-import shutil
-
+import json
+from datetime import datetime
 total = 0
 active = 0
 abandoned = 0
@@ -9,6 +9,26 @@ spam = 0
 rmdirs = 0
 
 blockchain = "ethereum"
+
+
+supported_chains = ['ethereum']
+ids_to_wipe = []
+new_search_tokens = []
+# TODO: use to cleanup tokens we don't want
+# for chain in supported_chains:
+#     f = open('blockchains/' + chain + '/tokenlist.json','r')
+#     tokenlist = json.load(f)
+#     search_tokens = tokenlist['search_tokens']
+#     print(len(search_tokens))
+#     for token in search_tokens:
+#         #print(token)
+#         if '24_hr_volume_usd' in token.keys() and token['24_hr_volume_usd'] > 0:
+#             new_search_tokens.append(token)
+
+#     tokenlist['search_tokens'] = new_search_tokens
+#     tokenlist['timestamp'] = str(datetime.now())
+#     f = open('blockchains/' + chain + '/tokenlist.json','w')
+#     f.write(json.dumps(tokenlist, indent=4))
 
 for path in Path('blockchains/' + blockchain + '/assets/').rglob('info.json'): 
   with open(path, 'r') as info:
